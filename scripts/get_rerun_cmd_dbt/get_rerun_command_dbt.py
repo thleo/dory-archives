ed289b4f5095278df918d8132fceb9d3ff87cf27
thleo <danielle.leong@ourzora.com>
2025-06-13 16:42:33 -0400

    update paths add additional keywords to ignore

--- scripts/get_rerun_cmd_dbt/get_rerun_command_dbt.py
+++ scripts/get_rerun_cmd_dbt/get_rerun_command_dbt.py
@@ -3,7 +3,7 @@ import re
 os.getcwd()
 
 # location of cloudwatch logs
-log_file_path = '/Users/dleong/Downloads/log-events-viewer-result.csv'
+log_file_path = 'console_logs.txt'
 
 # if pasting logs from console
 paste_logs = """"""
@@ -12,10 +12,11 @@ sep='------------------------------------------'
 
 models_to_rerun=[]
 except_regex_string=''
-except_words = ['logging','packages','github','build','user','install','pip','START','OK']
+except_words = ['logging','packages','github','build','user','install','pip','START','OK', 'cloud', '\.sql', 'yml', 'warn' , 'dataset', 'seeds']
 except_regex_string = '|'.join(except_words)
 prod_run_var_string = 'ENVIRONMENT=production AWS_PROFILE=production dbt run -m '
-dbt_logs_start = False
+# Change this variable if your logs include the start of your dbt run
+dbt_logs_start = True
 
 CWLogs = open(log_file_path, 'r')
 CW_lines = CWLogs.readline()
@@ -40,14 +41,19 @@ else:
         CW_lines = CWLogs.readline()
         if re.search('Running with dbt',CW_lines):
             dbt_logs_start=True
-        elif re.search('on-run-end hooks',CW_lines):
+        elif re.search('on-run-end hooks',CW_lines): # <<< this prevents the script from returning an output if the logs are pasted past the start of the dbt commands
             dbt_logs_start=False
-        if dbt_logs_start:
-            model_name_re = re.search('[a-z\_]+\.{1}[a-z\_\d]+',CW_lines)
+        if dbt_logs_start: # << need to fix this logic to work also when there isn't hte start line in the logs
+            model_name_re = re.search('[a-z\_]+\.{1}[a-z\_\d]+',CW_lines) 
             if model_name_re:
+                # TODO: fix regex to exclude models with numbers in names
                 if not (re.search(except_regex_string,CW_lines)):
                     model_name = model_name_re.group()
                     models_to_rerun.append(model_name)
+                    # uncomment to test outputs
+                # else:
+                    print(model_name_re.group())
+                    print(CW_lines)
 
 CWLogs.close()  
 
